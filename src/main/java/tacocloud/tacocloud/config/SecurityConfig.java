4f95bcfdc804ebae2961b55e890e9c58df66543e
PierreCorvus <aleksandrbasangov.1994@gmail.com>
2026-05-08 22:42:33 +0300

    fix: Omnipresent401AuthorisationFix

--- src/main/java/tacocloud/tacocloud/config/SecurityConfig.java
+++ src/main/java/tacocloud/tacocloud/config/SecurityConfig.java
@@ -2,14 +2,42 @@ package tacocloud.tacocloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/error/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/taco/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/ingredient/**").permitAll()
+                        .anyRequest().permitAll()// Protect all other endpoints
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
 }
