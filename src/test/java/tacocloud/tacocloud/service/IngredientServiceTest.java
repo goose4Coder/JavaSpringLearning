b27cb8d5654b144a086f8a0d5910c249bf4d160e
PierreCorvus <aleksandrbasangov.1994@gmail.com>
2026-05-14 22:10:22 +0300

    test: tacoAndIngredientServiceFull

--- src/test/java/tacocloud/tacocloud/service/IngredientServiceTest.java
+++ src/test/java/tacocloud/tacocloud/service/IngredientServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import tacocloud.tacocloud.dto.IngredientCategory;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,6 +44,19 @@ public class IngredientServiceTest {
     }
 
     @Test
+    public void testById_nullOnInexistentId(){
+        List<IngredientEntity> toTestWith= new ArrayList<IngredientEntity>();
+        toTestWith.add(IngredientEntity.builder().name("Secret sauce").id(0L).category("SAUCE").build());
+        toTestWith.add(IngredientEntity.builder().name("Wheat tortilla").id(1L).category("BREAD").build());
+        Mockito.lenient().when(repository.findById(anyLong())).thenReturn(Optional.empty());
+        Mockito.lenient().when(repository.findById(1L)).thenReturn(Optional.of(toTestWith.get(1)));
+        Mockito.lenient().when(repository.findById(0L)).thenReturn(Optional.of(toTestWith.get(0)));
+        Optional<IngredientDto> toCheck = service.byId(7);
+        Assertions.assertTrue(toCheck.isEmpty());
+
+    }
+
+    @Test
     public void createIngredient_createsCorrectly(){
         IngredientEntity entity=IngredientEntity.builder().name("Secret sauce").id(1L).category("SAUCE").build();
         IngredientDto dto= IngredientDto.builder().name("Secret sauce").category(IngredientCategory.SAUCE).id(2L).build();
@@ -52,6 +67,18 @@ public class IngredientServiceTest {
         Assertions.assertEquals("Secret sauce", toCheck.getName());
 
     }
+    @Test
+    public void updateIngredient_updatesCorrectly(){
+        IngredientEntity entity=IngredientEntity.builder().name("Secret sauce").id(1L).category("SAUCE").build();
+        IngredientDto dto= IngredientDto.builder().name("Secret sauce").category(IngredientCategory.SAUCE).id(2L).build();
+        when(repository.save(any(IngredientEntity.class))).thenReturn(entity);
+        IngredientDto toCheck = service.updateIngredient(1,dto);
+        Assertions.assertEquals(1, toCheck.getId());
+        Assertions.assertEquals(IngredientCategory.SAUCE, toCheck.getCategory());
+        Assertions.assertEquals("Secret sauce", toCheck.getName());
+
+    }
+
 
 
 
---
+++ src/test/java/tacocloud/tacocloud/service/TacoServiceTest.java
@@ -0,0 +1,110 @@
+package tacocloud.tacocloud.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tacocloud.tacocloud.dto.IngredientCategory;
+import tacocloud.tacocloud.dto.IngredientDto;
+import tacocloud.tacocloud.dto.TacoDto;
+import tacocloud.tacocloud.dto.TacoSize;
+import tacocloud.tacocloud.entity.IngredientEntity;
+import tacocloud.tacocloud.entity.TacoEntity;
+import tacocloud.tacocloud.mapper.IngredientMapper;
+import tacocloud.tacocloud.mapper.TacoMapper;
+import tacocloud.tacocloud.repository.TacoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class TacoServiceTest {
+    @Mock
+    private TacoRepository repository;
+    @InjectMocks
+    private TacoService service;
+
+    @Test
+    public void testById_givesTacoCorrectly(){
+        List<IngredientEntity> ingredients = new ArrayList<IngredientEntity>();
+        ingredients.add(IngredientEntity.builder().name("Secret sauce").id(0L).category("SAUCE").build());
+        ingredients.add(IngredientEntity.builder().name("Wheat tortilla").id(1L).category("BREAD").build());
+        TacoEntity taco = TacoEntity.builder().name("Empty sauced taco").size("LARGE").id(1L).ingredients(ingredients).build();
+        TacoEntity taco2 = TacoEntity.builder().name("Empty sauced taco2").size("BIG").id(1L).ingredients(ingredients).build();
+        when(repository.findById(1L)).thenReturn(Optional.of(taco));
+        when(repository.findById(2L)).thenReturn(Optional.of(taco2));
+        Optional<TacoDto> toCheck = service.byId(1L);
+        Optional<TacoDto> toCheck2 = service.byId(2L);
+        Assertions.assertTrue(toCheck.isPresent());
+        Assertions.assertEquals(1, toCheck.get().getId());
+        Assertions.assertEquals("Empty sauced taco", toCheck.get().getName());
+        Assertions.assertEquals(TacoSize.LARGE, toCheck.get().getSize());
+        Assertions.assertEquals(ingredients.stream().map(IngredientMapper::toDto).toList(), toCheck.get().getIngredients());
+
+        Assertions.assertTrue(toCheck2.isPresent());
+        Assertions.assertEquals(1, toCheck2.get().getId());
+        Assertions.assertEquals("Empty sauced taco2", toCheck2.get().getName());
+        Assertions.assertEquals(TacoSize.LARGE, toCheck2.get().getSize());
+        Assertions.assertEquals(ingredients.stream().map(IngredientMapper::toDto).toList(), toCheck2.get().getIngredients());
+
+    }
+
+    @Test
+    public void createTaco_createsTacoCorrectly(){
+        List<IngredientEntity> ingredients = new ArrayList<IngredientEntity>();
+        ingredients.add(IngredientEntity.builder().name("Secret sauce").id(0L).category("SAUCE").build());
+        ingredients.add(IngredientEntity.builder().name("Wheat tortilla").id(1L).category("BREAD").build());
+        TacoEntity taco = TacoEntity.builder().name("Empty sauced taco").size("MEDIUM").id(1L).ingredients(ingredients).build();
+        TacoDto dto = TacoDto.builder()
+                .name("Empty sauced taco").size(TacoSize.MEDIUM).id(0L).ingredients(ingredients.stream().map(IngredientMapper::toDto).toList()).build();
+        when(repository.save(any(TacoEntity.class))).thenReturn(taco);
+        TacoDto toCheck = service.createTaco(dto);
+        Assertions.assertEquals(1, toCheck.getId());
+        Assertions.assertEquals("Empty sauced taco", toCheck.getName());
+        Assertions.assertEquals(TacoSize.MEDIUM, toCheck.getSize());
+        Assertions.assertEquals(ingredients.stream().map(IngredientMapper::toDto).toList(), toCheck.getIngredients());
+
+    }
+
+    @Test
+    public void updateTaco_updatesTacoCorrectly(){
+        List<IngredientEntity> ingredients = new ArrayList<IngredientEntity>();
+        ingredients.add(IngredientEntity.builder().name("Secret sauce").id(0L).category("SAUCE").build());
+        ingredients.add(IngredientEntity.builder().name("Wheat tortilla").id(1L).category("BREAD").build());
+        TacoEntity taco = TacoEntity.builder().name("Empty sauced taco").size("MEDIUM").id(1L).ingredients(ingredients).build();
+        TacoDto dto = TacoDto.builder()
+                .name("Empty sauced taco").size(TacoSize.MEDIUM).id(4L).ingredients(ingredients.stream().map(IngredientMapper::toDto).toList()).build();
+        when(repository.save(any(TacoEntity.class))).thenReturn(taco);
+        TacoDto toCheck = service.updateTaco(1L,dto);
+        Assertions.assertEquals(1, toCheck.getId());
+        Assertions.assertEquals("Empty sauced taco", toCheck.getName());
+        Assertions.assertEquals(TacoSize.MEDIUM, toCheck.getSize());
+        Assertions.assertEquals(ingredients.stream().map(IngredientMapper::toDto).toList(), toCheck.getIngredients());
+
+    }
+    @Test
+    public void testById_nullOnInexistentId(){
+        List<IngredientEntity> ingredients = new ArrayList<IngredientEntity>();
+        ingredients.add(IngredientEntity.builder().name("Secret sauce").id(0L).category("SAUCE").build());
+        ingredients.add(IngredientEntity.builder().name("Wheat tortilla").id(1L).category("BREAD").build());
+        TacoEntity taco = TacoEntity.builder().name("Empty sauced taco").size("LARGE").id(1L).ingredients(ingredients).build();
+        TacoEntity taco2 = TacoEntity.builder().name("Empty sauced taco2").size("BIG").id(5L).ingredients(ingredients).build();
+        Mockito.lenient().when(repository.findById(anyLong())).thenReturn(Optional.empty());
+        Mockito.lenient().when(repository.findById(1L)).thenReturn(Optional.of(taco));
+        Mockito.lenient().when(repository.findById(5L)).thenReturn(Optional.of(taco2));
+        Optional<TacoDto> toCheck = service.byId(3L);
+        Assertions.assertTrue(toCheck.isEmpty());
+
+
+    }
+
+
+}
